ad7fc70b5764a0c27e9303e0c043a7fdf2b14dde
joycechendev <superbrainchen@gmail.com>
2018-02-05 21:44:30 -0700

    Add word based k gram generation

---
+++ src/com/company/RandomGeneration.java
@@ -0,0 +1,126 @@
+package com.company;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Created by super on 2/5/2018.
+ */
+public class RandomGeneration {
+    public void measureTime(int m) throws IOException {
+        String fileName = "Coupon" + Integer.toString(m) + "Times";
+
+        PrintWriter writer = new PrintWriter(fileName, "UTF-8");
+
+        for (int i = 250; i <= 4000; i+=500) {
+            long startTime = System.currentTimeMillis();
+            for(int j = 1; j <= m; j++) {
+                runMultipleNoFile(i, j);
+            }
+            long stopTime = System.currentTimeMillis();
+            long elapsedTime = stopTime - startTime;
+            writer.print(i); // aka n
+            writer.print(" ");
+            writer.print(elapsedTime);
+            writer.println();
+        }
+        writer.close();
+    }
+    public int genRandom(int n) {
+        HashSet<Integer> h = new HashSet<Integer>();
+        Random rand = new Random();
+        int r = rand.nextInt(n) + 1;
+        int k = 1;
+        while(!h.contains(r)) {
+            h.add(r);
+            r = rand.nextInt(n) + 1;
+            k++;
+        }
+
+        return k;
+    }
+
+    // coupon collector
+    public int collectCoupon(int n) {
+        HashSet<Integer> h = new HashSet<Integer>();
+        Random rand = new Random();
+        int r = rand.nextInt(n) + 1;
+        int k = 1;
+        while(h.size() != n) {
+            h.add(r);
+            r = rand.nextInt(n) + 1;
+            k++;
+        }
+
+        return k;
+    }
+
+    // run m experiments with input n
+    public void runMultiple(int n, int m) throws IOException {
+        PrintWriter writer = new PrintWriter("Coupon300TimesNoTime", "UTF-8");
+        for (int i = 1; i <= m; i++) {
+            int k = collectCoupon(n);
+            writer.println(k);
+        }
+        writer.close();
+    }
+
+    public void runMultipleNoFile(int n, int m) {
+        for (int i = 1; i <= m; i++) {
+            int k = collectCoupon(n);
+        }
+    }
+
+    public void calCumulative(String fileName) throws IOException {
+        File file = new File(fileName);
+        Scanner sc = new Scanner(file);
+        int[] res = new int[21];
+        float[] resProb = new float[21];
+        int[] raw = new int[300];
+        int index = 0;
+
+        while(sc.hasNext()) {
+            // raw[index++] = sc.nextInt();
+            //res[sc.nextInt()/10]++;
+            int n = sc.nextInt();
+            if(n/10 < 20) {
+                res[n/10]++;
+            }
+            //System.out.println(sc.nextInt()/10);
+        }
+
+        res[20] = 300;
+        int sum = res[0];
+        for(int i = 1; i < res.length; i++) {
+            //System.out.println(res[i]);
+            sum = sum+res[i];
+            System.out.println(sum);
+            resProb[i] = (float)sum/300;
+        }
+
+        resProb[20] = 1;
+        PrintWriter writer = new PrintWriter("CollisionDensity", "UTF-8");
+        for (int i = 0; i < resProb.length; i++) {
+            writer.print(i*10); // aka n
+            writer.print(" ");
+            writer.print(resProb[i]);
+            writer.println();;
+        }
+
+        writer.close();
+    }
+    public int calAverage(String fileName, int n) throws IOException {
+        File file = new File(fileName);
+        Scanner sc = new Scanner(file);
+        int sum = 0;
+        while(sc.hasNextInt()) {
+            sum += sc.nextInt();
+        }
+
+        return sum/n;
+    }
+}
--- src/com/company/kGram.java
+++ src/com/company/kGram.java
@@ -2,6 +2,7 @@ package com.company;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Scanner;
 
@@ -9,17 +10,29 @@ import java.util.Scanner;
  * Created by super on 2/3/2018.
  */
 public class kGram {
-    public HashSet<String> kGramCharBased(String filename, int k) throws IOException {
-        HashSet<String> set = new HashSet<String>();
+    public HashSet<ArrayList<String>> kGramWordBased(String filename, int k) throws IOException {
+        HashSet<ArrayList<String>> set = new HashSet<ArrayList<String>>();
+        ArrayList<String> rawSet = new  ArrayList<String>();
         File file = new File(filename);
-        Scanner sc = new Scanner(file).useDelimiter("\\s+");;
+        Scanner sc = new Scanner(file).useDelimiter("\\s+");
         while(sc.hasNext()) {
-
+           //int temp = k;
+            rawSet.add(sc.next());
         }
+
+        for(int i = 0; i <= rawSet.size() - k; i++) {
+            ArrayList<String> curr = new ArrayList<String>();
+            for(int j = 0; j < k; j++) {
+                curr.add(rawSet.get(i+j));
+            }
+            set.add(curr);
+        }
+
+        sc.close();
         return set;
     }
 
-    public HashSet<String> kGramWordBased(String filename) throws IOException {
+    public HashSet<String> kGramCharBased(String filename) throws IOException {
         HashSet<String> set = new HashSet<String>();
         File file = new File(filename);
         Scanner sc = new Scanner(file).useDelimiter("\\s+");
